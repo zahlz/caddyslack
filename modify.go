ab27720b6948d890a0fceff2efa2ff699632bf1a
Kevin Seidel <kevin@zahlz.com>
2016-11-03 08:41:15 +0100

    Remove prints and cleanup ifs
    Remove prints which were only necessary for debugging.
    Also reversed some if's to remove indentation.

--- modify.go
+++ modify.go
@@ -8,37 +8,38 @@ import (
 )
 
 func deleteJSONFromReader(readerIn io.Reader, pointsToDelete []string) (readerOut io.Reader, err error) {
-	if len(pointsToDelete) > 0 {
-		jsonParsed, err := gabs.ParseJSONBuffer(readerIn)
-		if err != nil {
-			return readerOut, err
-		}
-		for _, pointToDelete := range pointsToDelete {
-			jsonParsed.DeleteP(pointToDelete)
-		}
-		return bytes.NewReader(jsonParsed.Bytes()), nil
+	if len(pointsToDelete) <= 0 {
+		return readerIn, nil
 	}
-	return readerIn, nil
+
+	jsonParsed, err := gabs.ParseJSONBuffer(readerIn)
+	if err != nil {
+		return readerOut, err
+	}
+	for _, pointToDelete := range pointsToDelete {
+		jsonParsed.DeleteP(pointToDelete)
+	}
+	return bytes.NewReader(jsonParsed.Bytes()), nil
 }
 
 func onlyJSONFromReader(readerIn io.Reader, pointsOnly []string) (readerOut io.Reader, err error) {
-	if pointsOnly != nil {
-		jsonParsed, err := gabs.ParseJSONBuffer(readerIn)
-		if err != nil {
-			return readerOut, err
-		}
-		outContainer := gabs.New()
-		for _, point := range pointsOnly {
-			if jsonParsed.ExistsP(point) {
-				//fmt.Printf("%v: %v\n", point, jsonParsed.Path(point).Data())
-				_, err = outContainer.SetP(jsonParsed.Path(point).Data(), point)
-				if err != nil {
-					return readerOut, err
-				}
+	if pointsOnly == nil {
+		return readerIn, nil
+	}
+
+	jsonParsed, err := gabs.ParseJSONBuffer(readerIn)
+	if err != nil {
+		return readerOut, err
+	}
+
+	outContainer := gabs.New()
+	for _, point := range pointsOnly {
+		if jsonParsed.ExistsP(point) {
+			_, err = outContainer.SetP(jsonParsed.Path(point).Data(), point)
+			if err != nil {
+				return readerOut, err
 			}
 		}
-		//fmt.Printf("container %#v\n", outContainer)
-		return bytes.NewReader(outContainer.Bytes()), nil
 	}
-	return readerIn, nil
+	return bytes.NewReader(outContainer.Bytes()), nil
 }
